991f7fb818509e1bc9992c69581841f9d00ff8d5
David Avedissian <davedissian@users.noreply.github.com>
2015-01-08 15:48:17 +0000

    Hopefully made the EXCEPT macro compatible with 1.9 stable

--- Plugins/ParticleUniverse/include/ParticleUniverseException.h
+++ Plugins/ParticleUniverse/include/ParticleUniverseException.h
@@ -29,7 +29,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 namespace ParticleUniverse
 {
 	// If the Ogre renderer is replaced by another renderer, the type below must be re-implemented.
-  typedef Ogre::Exception Exception;
-  #define EXCEPT(num, desc, src) OGRE_EXCEPT(num, desc, src);
+	typedef Ogre::Exception Exception;
+#ifdef OGRE_EXCEPT
+	#define EXCEPT(num, desc, src) OGRE_EXCEPT(num, desc, src);
+#else
+	#define EXCEPT(num, desc, src) throw Ogre::ExceptionFactory::create( \
+			Ogre::ExceptionCodeType<num>(), desc, src, __FILE__, __LINE__ );
+#endif
 }
 #endif
