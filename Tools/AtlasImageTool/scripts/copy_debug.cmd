19ac3172a54b4f842c69cbaf3207583bed59ebd9
Pavel Rojtberg <rojtberg@gmail.com>
2016-09-24 21:21:09 +0200

    remove binaries and vcproj leftovers

--- Tools/AtlasImageTool/scripts/copy_debug.cmd
+++
@@ -1,3 +0,0 @@
-copy /Y ..\..\..\bin\Debug\AtlasImageTool.exe ..\bin\Debug
-copy /Y ..\..\..\bin\Debug\ParticleUniverse_d.dll ..\bin\Debug
-copy /Y ..\..\..\bin\Debug\OgreMain_d.dll ..\bin\Debug
--- Tools/AtlasImageTool/scripts/copy_release.cmd
+++
@@ -1,3 +0,0 @@
-copy /Y ..\..\..\bin\Release\AtlasImageTool.exe ..\bin\Release
-copy /Y ..\..\..\bin\Release\ParticleUniverse.dll ..\bin\Release
-copy /Y ..\..\..\bin\Release\OgreMain.dll ..\bin\Release
