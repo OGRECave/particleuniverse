f69a321b25a7429ca24f8b3568e6716b4d31da52
Pavel Rojtberg <rojtberg@gmail.com>
2019-06-07 02:11:06 +0200

    Sample: add Trays based UI to browse particle systems

--- Samples/ParticleUniverseDemo/include/ParticleUniverseDemo.h
+++ Samples/ParticleUniverseDemo/include/ParticleUniverseDemo.h
@@ -10,17 +10,20 @@
 #include <OgreSceneNode.h>
 #include <OgreApplicationContext.h>
 #include <OgreCameraMan.h>
+#include <OgreTrays.h>
 
 #include "ParticleUniversePlugin.h"
 
 using namespace Ogre;
 
-class Sample_ParticleUniverseDemo : public OgreBites::ApplicationContext, public OgreBites::InputListener
+class Sample_ParticleUniverseDemo : public OgreBites::ApplicationContext, public OgreBites::InputListener, public OgreBites::TrayListener
 {
 public:
 	Ogre::SceneManager* mSceneMgr;
 	Ogre::Camera* mCamera;
 	OgreBites::CameraMan* mCameraMan;
+
+	OgreBites::TrayManager* mTrayMgr;
 
 	// ApplicationContext Boilerplate
 	Sample_ParticleUniverseDemo() : OgreBites::ApplicationContext("PU Demo") {
@@ -30,8 +33,6 @@ public:
 	    OgreBites::ApplicationContext::createRoot();
 	    getRoot()->installPlugin(new ParticleUniverse::ParticleUniversePlugin);
 	}
-
-	void setupInput(bool) {}
 
 	/** -----------------------------------------------------------------------
 	Create some particle systems
@@ -48,38 +49,39 @@ public:
 		addInputListener(mCameraMan);
 
 		mCamera->setFarClipDistance(10000);
-		mCamera->setNearClipDistance(0.1);
+		mCamera->setNearClipDistance(5);
 		Ogre::Viewport* viewport = getRenderWindow()->addViewport(mCamera);
+		viewport->setBackgroundColour(ColourValue(0.3, 0.3, 0.3));
 
+		mSceneMgr->addRenderQueueListener(getOverlaySystem());
+		
 		// setup some basic lighting for our scene
-        mSceneMgr->setAmbientLight(ColourValue(0.3, 0.3, 0.3));
-        //mSceneMgr->createLight()->setPosition(20, 80, 50);
+        mSceneMgr->setAmbientLight(ColourValue(0.1, 0.1, 0.1));
+        mSceneMgr->getRootSceneNode()->createChildSceneNode(Vector3(20, 80, 50))->attachObject(mSceneMgr->createLight());
         
 		// create a skyplane 5000 units away, facing down, 10000 square units large, with 3x texture tiling
         //mSceneMgr->setSkyPlane(true, Plane(0, -1, 0, 5000), "Examples/SpaceSkyPlane", 10000, 3);
 
 		// Create the particle systems and attach them to a SceneNode
 		ParticleUniverse::ParticleSystemManager* pManager = ParticleUniverse::ParticleSystemManager::getSingletonPtr();
-		ParticleUniverse::ParticleSystem* pSys1 = pManager->createParticleSystem("pSys1", "mp_torch", mSceneMgr);
-		ParticleUniverse::ParticleSystem* pSys2 = pManager->createParticleSystem("pSys2", "mp_torch", mSceneMgr);
-		mSceneMgr->getRootSceneNode()->attachObject(pSys1);
-		mSceneMgr->getRootSceneNode()->attachObject(pSys2);
+		ParticleUniverse::ParticleSystem* pSys = pManager->createParticleSystem("pSys1", "mp_torch", mSceneMgr);
+
+		mTrayMgr = new OgreBites::TrayManager("Interface", getRenderWindow(), this);
+		addInputListener(mTrayMgr);
+
+		std::vector<String> templateNames;
+		pManager->particleSystemTemplateNames(templateNames);
+        auto menu = mTrayMgr->createLongSelectMenu(OgreBites::TL_TOPLEFT, "MaterialSelectMenu", "Particle System", 350, 200, 15, templateNames);
+		menu->selectItem("mp_torch", false);
+
+		// Adjust the position of the particle systems a bit
+		mSceneMgr->getRootSceneNode()->attachObject(pSys);
 
 		// Scale the particle systems, just because we can!
-		pSys1->setScaleVelocity(10);
-		pSys1->setScale(Ogre::Vector3(10, 10, 10));
-		pSys2->setScaleVelocity(10);
-		pSys2->setScale(Ogre::Vector3(10, 10, 10));
-
-		// Adjust the position of the particle systems a bit by repositioning their ParticleTechnique (there is only one technique in mp_torch)
-		// Normally you would do that by setting the position of the SceneNode to which the Particle System is attached, but in this
-		// demo they are both attached to the same rootnode.
-		pSys1->getTechnique(0)->position = Ogre::Vector3(5, 0, 0);
-		pSys2->getTechnique(0)->position = Ogre::Vector3(-5, 0, 0);
-
+		pSys->setScaleVelocity(10);
+		pSys->setScale(Ogre::Vector3(10, 10, 10));
 		// Start!
-		pSys1->start();
-		pSys2->start();
+		pSys->start();
 
 		// Look at the torches
 		mCameraMan->setYawPitchDist(Radian(0), Radian(0), 150);
@@ -92,6 +94,19 @@ public:
 	{
 		mSceneMgr->getRootSceneNode()->detachAllObjects();
 		ParticleUniverse::ParticleSystemManager::getSingletonPtr()->destroyAllParticleSystems(mSceneMgr);
+	}
+
+	void itemSelected(OgreBites::SelectMenu* menu)
+	{
+		auto pManager = ParticleUniverse::ParticleSystemManager::getSingletonPtr();
+		auto templateName = menu->getSelectedItem().asUTF8();
+
+		pManager->destroyParticleSystem("pSys1", mSceneMgr);
+		auto pSys = pManager->createParticleSystem("pSys1", templateName, mSceneMgr);
+		mSceneMgr->getRootSceneNode()->attachObject(pSys);
+		pSys->setScaleVelocity(10);
+		pSys->setScale(Vector3(10, 10, 10));
+		pSys->start();
 	}
 
 /** -----------------------------------------------------------------------
@@ -108,7 +123,6 @@ public:
 		
 		ParticleUniverse::ParticleSystemManager* pManager = ParticleUniverse::ParticleSystemManager::getSingletonPtr();
 		ParticleUniverse::ParticleSystem* pSys1 = pManager->getParticleSystem("pSys1");
-		ParticleUniverse::ParticleSystem* pSys2 = pManager->getParticleSystem("pSys2");
 
 		if (evt.keysym.sym == SDLK_F9)
 		{
@@ -117,10 +131,6 @@ public:
 			{
 				pSys1->pause();
 			}
-			if (pSys2)
-			{
-				pSys2->pause();
-			}
 		}
 		else if (evt.keysym.sym == SDLK_F10)
 		{
@@ -128,10 +138,6 @@ public:
 			if (pSys1)
 			{
 				pSys1->resume();
-			}
-			if (pSys2)
-			{
-				pSys2->resume();
 			}
 		}
 		return true;
